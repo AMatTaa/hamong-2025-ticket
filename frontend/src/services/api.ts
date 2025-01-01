4c30b15d35fecf55fafc54c3284f629613297a5f
AMatTaa <relajar@naver.com>
2025-01-01 19:18:49 +0900

    a

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -14,11 +14,6 @@ export const api = {
           'Accept': 'application/json',
         },
         timeout: 10000, // 10 seconds timeout
-        retry: {
-          limit: 2,
-          methods: ['post'],
-          statusCodes: [408, 413, 429, 500, 502, 503, 504],
-        },
         hooks: {
           beforeRequest: [
             request => {
